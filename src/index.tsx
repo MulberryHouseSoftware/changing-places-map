0d6aa5f8ea2fff9e1a38edb4d6bfe162b5b40e11
johnwalley <john@walley.org.uk>
2021-09-17 15:25:57 +0100

    chore: upgrade web worker for react-scripts 4

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import "./index.css";
 import "@fontsource/roboto";
 
-import * as serviceWorker from "./serviceWorker";
-
 import {
   StylesProvider,
   ThemeProvider,
@@ -14,6 +12,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import PWAPrompt from "react-ios-pwa-prompt";
 import ReactDOM from "react-dom";
 import { defaultTheme } from "./theme/defaultTheme";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const theme = createTheme(defaultTheme);
 
@@ -55,5 +54,5 @@ document.head.appendChild(script);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -8,7 +8,7 @@
 // resources are updated in the background.
 
 // To learn more about the benefits of this model and instructions on how to
-// opt-in, read https://bit.ly/CRA-PWA
+// opt-in, read https://cra.link/PWA
 
 const isLocalhost = Boolean(
   window.location.hostname === "localhost" ||
@@ -29,7 +29,6 @@ export function register(config?: Config) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -49,7 +48,7 @@ export function register(config?: Config) {
         navigator.serviceWorker.ready.then(() => {
           console.log(
             "This web app is being served cache-first by a service " +
-              "worker. To learn more, visit https://bit.ly/CRA-PWA"
+              "worker. To learn more, visit https://cra.link/PWA"
           );
         });
       } else {
@@ -69,7 +68,6 @@ function registerValidSW(swUrl: string, config?: Config) {
         if (installingWorker == null) {
           return;
         }
-
         installingWorker.onstatechange = () => {
           if (installingWorker.state === "installed") {
             if (navigator.serviceWorker.controller) {
@@ -78,7 +76,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               // content until all client tabs are closed.
               console.log(
                 "New content is available and will be used when all " +
-                  "tabs for this page are closed. See https://bit.ly/CRA-PWA."
+                  "tabs for this page are closed. See https://cra.link/PWA."
               );
 
               // Execute callback
@@ -99,8 +97,6 @@ function registerValidSW(swUrl: string, config?: Config) {
           }
         };
       };
-
-      registration.update();
     })
     .catch((error) => {
       console.error("Error during service worker registration:", error);
