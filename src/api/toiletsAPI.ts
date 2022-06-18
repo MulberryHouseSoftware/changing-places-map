04f4a5a3638d2ef42e615d0df84c48a20944f1c2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-19 01:19:32 +0800

    Delete src directory

--- src/api/toiletsAPI.ts
+++
@@ -1,7 +0,0 @@
-export const readAll = async () => {
-  const response = await fetch("/.netlify/functions/read-all");
-
-  const json = await response.json();
-
-  return json.map((row: any) => ({ ...row.data, id: row.ref["@ref"].id }));
-};
